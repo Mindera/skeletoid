585ef01bcc841a1aa3c9e56d03057b45f8f87da9
Pedro Vicente <neteinstein@gmail.com>
2017-04-12 16:01:22 +0100

    Reachability - Added utils to validate network state. (#12)

---
+++ library/src/main/java/com/mindera/skeletoid/network/Connectivity.java
@@ -0,0 +1,48 @@
+package com.mindera.skeletoid.network;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Class to validate if we are connected to a network and if we have internet access.
+ */
+public class Connectivity {
+
+//    private static final String LOG_TAG = "Connectivity";
+
+    /**
+     * Validates if we are connected to a network and if we have real internet access
+     * NOTE: This will only work if ConnectivityReceiver is added to the app manifest
+     * @param context The context
+     * @return true if internet access is available, false otherwise.
+     */
+    public static boolean isConnectedAndWithInternetAvailable(Context context) {
+        return isConnected(context) && ConnectivityReceiver.isInternetAvailable;
+    }
+
+    /**
+     * Validates if we are connected to a network.
+     * WARNING: Being connected to a network DOES NOT imply internet access!
+     * @param context The context
+     * @return true if we are connected, false otherwise.
+     */
+    public static boolean isConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        return ni != null;
+    }
+
+    /**
+     * Check if we are connected to a WIFI network
+     * @param context The context
+     * @return true if we are connected, false otherwise.
+     */
+    public static boolean isConnectedToWIFI(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        return ni != null && ni.getType() == ConnectivityManager.TYPE_WIFI;
+    }
+}
---
+++ library/src/main/java/com/mindera/skeletoid/network/ConnectivityReceiver.java
@@ -0,0 +1,127 @@
+package com.mindera.skeletoid.network;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+
+import com.mindera.skeletoid.logs.LOG;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * This class updates the state of the connection according to real http requests to Google
+ *
+ * It's just a helper class and shouldn't be directly accessed, but via the Connectivity class
+ *
+ * To use it must have a Receiver defined in the Manifest:
+ * <pre>
+ * {@code
+ * <receiver android:name="com.mindera.skeletoid.network.ConnectivityReceiver">
+ * <intent-filter>
+ * <action android:name="android.net.conn.CONNECTIVITY_CHANGE" />
+ * <action android:name="android.net.wifi.STATE_CHANGE"/>
+ * <action android:name="android.net.wifi.supplicant.CONNECTION_CHANGE"/>
+ * </intent-filter>
+ * </receiver>
+ * }
+ * </pre>
+ */
+public class ConnectivityReceiver extends BroadcastReceiver {
+
+    private static final String LOG_TAG = "ConnectivityReceiver";
+    /**
+     * Var to control if the network was reachable before
+     */
+    private static Boolean mPreviousReachableState = null;
+    /**
+     * Var that contains the current state of connectivity
+     */
+    protected static volatile boolean isInternetAvailable = false;
+
+    private static final String mInternetAddress = "http://www.google.com";
+    private static final String mInternetHttpValidationAddress = "http://www.google";
+    private static final String mInternetHttpsValidationAddress = "https://www.google";
+    private static final int mInternetValidationTimeout = 1000;
+
+    private static final int HTTP_200 = 200;
+    private static final int HTTP_201 = 201;
+
+    private static final long ONE_SECOND = 1000;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        final ConnectivityManager connMgr = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        final NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        boolean isReachable = networkInfo != null && networkInfo.isConnected();
+
+        if (isReachable) {
+            updateInternetStatus(3);
+        }
+        isInternetAvailable = isReachable;
+
+        if (mPreviousReachableState == null || mPreviousReachableState != isReachable) {
+
+            mPreviousReachableState = isReachable;
+
+            //TODO PUSH LOCAL BROADCAST WITH NEW STATE OR ADD POSSIBILITY TO ADD CALLBACK
+
+            LOG.d(LOG_TAG, "Connectivity changed " + isReachable);
+        }
+    }
+
+    /**
+     * Check if internet is available if we can open a http connection to google
+     *
+     * @param numberRetries Number of retries of this request.
+     */
+    public static void updateInternetStatus(final int numberRetries) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                int retries = numberRetries;
+                do {
+                    try {
+                        URL url = new URL(mInternetAddress);
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                        conn.setReadTimeout(mInternetValidationTimeout);
+                        isInternetAvailable =
+                                conn.getResponseCode() == HTTP_200 || conn.getResponseCode() == HTTP_201;
+
+                        LOG.d(LOG_TAG, "Response code -> " + conn.getResponseCode());
+
+                        isInternetAvailable &= conn
+                                .getURL().toString()
+                                .startsWith(mInternetHttpValidationAddress) || conn
+                                .getURL().toString()
+                                .startsWith(mInternetHttpsValidationAddress);
+
+                        LOG.d(LOG_TAG, "URL -> ", conn.getURL().toString());
+
+                        conn.disconnect();
+
+                        if (isInternetAvailable) {
+                            LOG.d(LOG_TAG, "INTERNET IS AVAILABLE");
+                            retries = 0;
+                        } else {
+                            LOG.d(LOG_TAG, "INTERNET IS NOT AVAILABLE");
+                            retries--;
+                            Thread.sleep(ONE_SECOND);
+                        }
+                    } catch (Exception ex) {
+                        LOG.e(LOG_TAG, ex, "ConnectivityReceiver isConnected");
+                        isInternetAvailable = false;
+                    }
+
+                } while (retries > 0);
+                return null;
+            }
+        }.execute();
+    }
+}
