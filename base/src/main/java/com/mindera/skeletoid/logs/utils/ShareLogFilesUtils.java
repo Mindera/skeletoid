dfb5b09f6908f4d64fbd170ba4c703efc58f9b1b
Renato Almeida <renato.m.alm@gmail.com>
2018-07-09 22:28:31 +0100

    Merge all Skeletoids (#61)
    * Merge skeletoid in a single repo: updated travis file, unit tests, codacy
    
    * Update .travis.yml

---
+++ base/src/main/java/com/mindera/skeletoid/logs/utils/ShareLogFilesUtils.java
@@ -0,0 +1,181 @@
+package com.mindera.skeletoid.logs.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.VisibleForTesting;
+import android.support.v4.content.FileProvider;
+
+import com.mindera.skeletoid.generic.AndroidUtils;
+import com.mindera.skeletoid.logs.LOG;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * A way to share log file easily
+ * <p>
+ * Remember to add this to the Android Manifest of the App
+ * <p>
+ * <provider
+ * android:name="android.support.v4.content.FileProvider"
+ * android:authorities="${applicationId}"
+ * android:exported="false"
+ * android:grantUriPermissions="true">
+ * <meta-data
+ * android:name="android.support.FILE_PROVIDER_PATHS"
+ * android:resource="@xml/fileprovider"/>
+ * </provider>
+ * <p>
+ * and add the file fileprovider.xml to the resources with
+ * <p>
+ * <?xml version="1.0" encoding="utf-8"?>
+ * <paths>
+ * <files-path
+ * path="."
+ * name="logs" />
+ * </paths>
+ */
+public class ShareLogFilesUtils {
+
+    private static final String TAG = "ShareLogFilesUtils";
+
+    private static final String FOLDER_LOGS = "logs";
+    private static final String FILE_LOG_ZIP = "logs.zip";
+
+    @VisibleForTesting
+    ShareLogFilesUtils() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Class to be able to share LogFileAppender generated files
+     *
+     * @param activity           The activity
+     * @param intentChooserTitle Intent chooser title
+     * @param subjectTitle       Subject title (for email)
+     * @param bodyText           Body text
+     * @param emails             Emails to add on to: field (for email)
+     * @param file               Log file to be sent
+     */
+    public static void sendLogs(Activity activity, String intentChooserTitle, String subjectTitle,
+            String bodyText, String[] emails, File file) {
+
+
+        final Intent intent;
+
+        //TODO Currently this only supports 1 file. The code commented would support multiple.
+//        if (uris.size() == 1) {
+        intent = new Intent(Intent.ACTION_SEND);
+//        }
+//        else {
+//            intent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+//            intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+//        }
+//        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{""});
+        intent.putExtra(Intent.EXTRA_SUBJECT, subjectTitle);
+
+        // Add emails to show on to: field
+        if (emails != null && emails.length > 0) {
+            intent.putExtra(Intent.EXTRA_EMAIL, emails);
+        }
+
+        // Add additional information to the email
+        intent.putExtra(Intent.EXTRA_TEXT, bodyText);
+
+        if (file != null) {
+            Uri uri = FileProvider.getUriForFile(activity, activity.getPackageName(), file);
+
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.putExtra(Intent.EXTRA_STREAM, uri);
+            intent.setType(activity.getContentResolver().getType(uri));
+        } else {
+            intent.setType("text/plain");
+        }
+        activity.startActivity(Intent.createChooser(intent, intentChooserTitle));
+    }
+
+    /**
+     * Class to be able to share compressed LogFileAppender generated files
+     *
+     * @param activity           The activity
+     * @param intentChooserTitle Intent chooser title
+     * @param subjectTitle       Subject title (for email)
+     * @param bodyText           Body text
+     * @param emails             Emails to add on to: field (for email)
+     */
+    public static void sendLogsEmail(Activity activity, String intentChooserTitle, String subjectTitle,
+                                     String bodyText, String[] emails) {
+
+        File output = new File(getCompressedLogsPath(activity));
+        if (!zipLogFiles(getFileLogPath(activity) + File.separator, output.getAbsolutePath()) || !output.exists()) {
+            return;
+        }
+
+        sendLogs(activity, intentChooserTitle, subjectTitle, bodyText, emails, output);
+    }
+
+    /**
+     * Returns the path where the application logs are being stored.
+     *
+     * @param context Application context
+     *
+     * @return path   The default path where the application logs are being stored
+     *
+     * @see AndroidUtils
+     */
+    public static String getFileLogPath(Context context) {
+        return AndroidUtils.getFileDirPath(context, "");
+    }
+
+    private static String getCompressedLogsPath(Context context) {
+        String path = getFileLogPath(context) + File.separator + FOLDER_LOGS;
+        ensureFolderExists(path);
+
+        return path + File.separator + FILE_LOG_ZIP;
+    }
+
+    private static void ensureFolderExists(String path) {
+        new File(path).mkdirs();
+    }
+
+    private static boolean zipLogFiles(String source, String output) {
+        try {
+            FileOutputStream fos = new FileOutputStream(output);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+            File srcFile = new File(source);
+            File[] files = srcFile.listFiles();
+
+            LOG.d(TAG, "Compress directory: " + srcFile.getName() + " via zip.");
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    continue;
+                }
+
+                LOG.d(TAG, "Adding file: " + file.getName());
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = new FileInputStream(file);
+                zos.putNextEntry(new ZipEntry(file.getName()));
+
+                int length;
+                while ((length = fis.read(buffer)) > 0) {
+                    zos.write(buffer, 0, length);
+                }
+
+                zos.closeEntry();
+                fis.close();
+            }
+
+            zos.close();
+            return true;
+        } catch (IOException ex) {
+            LOG.e(TAG, "Unable to zip folder: " + ex.getMessage());
+            return false;
+        }
+    }
+}
---
+++ base/src/test/java/com/mindera/skeletoid/logs/LoggerManagerUnitTest.java
@@ -0,0 +1,342 @@
+package com.mindera.skeletoid.logs;
+
+import android.content.Context;
+
+import com.mindera.skeletoid.logs.appenders.ILogAppender;
+import com.mindera.skeletoid.logs.utils.LogAppenderUtils;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.mindera.skeletoid.logs.LoggerManager.LOG_FORMAT_4ARGS;
+import static com.mindera.skeletoid.logs.utils.LogAppenderUtils.getObjectHash;
+import static com.mindera.skeletoid.threads.utils.ThreadUtils.getCurrentThreadName;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class LoggerManagerUnitTest {
+
+    /**
+     * Should be the same as {@link LoggerManager#LOG_FORMAT_4ARGS}
+     */
+    private static final String LOG_FORMAT_4ARGS = "%s %s %s | %s";
+
+    private final String TAG = "TAG";
+    private final String TEXT = "Text";
+    private String mPackageName = "my.package.name";
+
+    @Test
+    public void testAddAppendersNull() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+        Set<String> appendersIds = loggerManager.addAppenders(context, null);
+
+        assertNotNull(appendersIds);
+        assertEquals(0, appendersIds.size());
+    }
+
+    @Test
+    public void testAddAppendersEmpty() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+        Set<String> appendersIds = loggerManager.addAppenders(context, new ArrayList<ILogAppender>());
+
+        assertNotNull(appendersIds);
+        assertEquals(0, appendersIds.size());
+    }
+
+    @Test
+    public void testAddAppenders() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+
+        List<ILogAppender> appenders = new ArrayList<>();
+
+        ILogAppender appenderA = mockAppender("A");
+        ILogAppender appenderB = mockAppender("B");
+        ILogAppender appenderC = mockAppender("C");
+
+        appenders.add(appenderA);
+        appenders.add(appenderB);
+        appenders.add(appenderC);
+
+        Set<String> appendersIds = loggerManager.addAppenders(context, appenders);
+
+        verify(appenderA, times(1)).enableAppender(context);
+        verify(appenderB, times(1)).enableAppender(context);
+        verify(appenderC, times(1)).enableAppender(context);
+
+        assertNotNull(appendersIds);
+        assertEquals(3, appendersIds.size());
+        assertTrue(appendersIds.contains("A"));
+        assertTrue(appendersIds.contains("B"));
+        assertTrue(appendersIds.contains("C"));
+    }
+
+    @Test
+    public void testAddAppendersRepeated() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+
+        List<ILogAppender> appenders = new ArrayList<>();
+
+        ILogAppender appenderA = mockAppender("A");
+        ILogAppender appenderB1 = mockAppender("B");
+        ILogAppender appenderB2 = mockAppender("B");
+
+        appenders.add(appenderA);
+        appenders.add(appenderB1);
+        appenders.add(appenderB2);
+
+        Set<String> appendersIds = loggerManager.addAppenders(context, appenders);
+
+        assertNotNull(appendersIds);
+        assertEquals(2, appendersIds.size());
+        assertTrue(appendersIds.contains("A"));
+        assertTrue(appendersIds.contains("B"));
+    }
+
+    @Test
+    public void testDisableAppendersNull() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+        loggerManager.removeAppenders(context, null);
+    }
+
+    @Test
+    public void testDisableAppendersEmpty() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+        loggerManager.removeAppenders(context, new HashSet<String>());
+    }
+
+    @Test
+    public void testDisableAppenders() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+
+        List<ILogAppender> appenders = new ArrayList<>();
+
+        ILogAppender appenderA = mockAppender("A");
+        ILogAppender appenderB = mockAppender("B");
+        ILogAppender appenderC = mockAppender("C");
+
+        appenders.add(appenderA);
+        appenders.add(appenderB);
+        appenders.add(appenderC);
+
+        Set<String> appendersIds = loggerManager.addAppenders(context, appenders);
+
+        loggerManager.removeAppenders(context, appendersIds);
+        verify(appenderA, times(1)).disableAppender();
+        verify(appenderB, times(1)).disableAppender();
+        verify(appenderC, times(1)).disableAppender();
+    }
+
+    @Test
+    public void testRemoveAllAppenders() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+
+        List<ILogAppender> appenders = new ArrayList<>();
+
+        ILogAppender appenderA = mockAppender("A");
+        ILogAppender appenderB = mockAppender("B");
+        ILogAppender appenderC = mockAppender("C");
+
+        appenders.add(appenderA);
+        appenders.add(appenderB);
+        appenders.add(appenderC);
+
+        loggerManager.addAppenders(context, appenders);
+
+        loggerManager.removeAllAppenders();
+        verify(appenderA, times(1)).disableAppender();
+        verify(appenderB, times(1)).disableAppender();
+        verify(appenderC, times(1)).disableAppender();
+    }
+
+    @Test
+    public void testDebugLog() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+
+        List<ILogAppender> appenders = new ArrayList<>();
+
+        ILogAppender appenderA = mockAppender("A");
+        ILogAppender appenderB = mockAppender("B");
+        ILogAppender appenderC = mockAppender("C");
+
+        appenders.add(appenderA);
+        appenders.add(appenderB);
+        appenders.add(appenderC);
+
+        loggerManager.addAppenders(context, appenders);
+
+        loggerManager.log(TAG, LOG.PRIORITY.DEBUG, TEXT);
+
+        final String log = String.format(LOG_FORMAT_4ARGS, TAG, getObjectHash(TAG), getCurrentThreadName(), LogAppenderUtils.getLogString(TEXT));
+
+        verify(appenderA, times(1)).log(LOG.PRIORITY.DEBUG, null, log);
+        verify(appenderB, times(1)).log(LOG.PRIORITY.DEBUG, null, log);
+        verify(appenderC, times(1)).log(LOG.PRIORITY.DEBUG, null, log);
+    }
+
+    @Test
+    public void testErrorLog() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+
+        List<ILogAppender> appenders = new ArrayList<>();
+
+        ILogAppender appenderA = mockAppender("A");
+        ILogAppender appenderB = mockAppender("B");
+        ILogAppender appenderC = mockAppender("C");
+
+        appenders.add(appenderA);
+        appenders.add(appenderB);
+        appenders.add(appenderC);
+
+        loggerManager.addAppenders(context, appenders);
+
+        loggerManager.log(TAG, LOG.PRIORITY.ERROR, TEXT);
+
+        final String log = String.format(LOG_FORMAT_4ARGS, TAG, getObjectHash(TAG), getCurrentThreadName(), LogAppenderUtils.getLogString(TEXT));
+
+        verify(appenderA, times(1)).log(LOG.PRIORITY.ERROR, null, log);
+        verify(appenderB, times(1)).log(LOG.PRIORITY.ERROR, null, log);
+        verify(appenderC, times(1)).log(LOG.PRIORITY.ERROR, null, log);
+    }
+
+    @Test
+    public void testWarnLog() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+        List<ILogAppender> appenders = new ArrayList<>();
+
+        ILogAppender appenderA = mockAppender("A");
+        ILogAppender appenderB = mockAppender("B");
+        ILogAppender appenderC = mockAppender("C");
+
+        appenders.add(appenderA);
+        appenders.add(appenderB);
+        appenders.add(appenderC);
+
+        loggerManager.addAppenders(context, appenders);
+
+        loggerManager.log(TAG, LOG.PRIORITY.WARN, TEXT);
+
+        final String log = String.format(LOG_FORMAT_4ARGS, TAG, getObjectHash(TAG), getCurrentThreadName(), LogAppenderUtils.getLogString(TEXT));
+
+        verify(appenderA, times(1)).log(LOG.PRIORITY.WARN, null, log);
+        verify(appenderB, times(1)).log(LOG.PRIORITY.WARN, null, log);
+        verify(appenderC, times(1)).log(LOG.PRIORITY.WARN, null, log);
+    }
+
+    @Test
+    public void testFatalLog() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+        List<ILogAppender> appenders = new ArrayList<>();
+
+        ILogAppender appenderA = mockAppender("A");
+        ILogAppender appenderB = mockAppender("B");
+        ILogAppender appenderC = mockAppender("C");
+
+        appenders.add(appenderA);
+        appenders.add(appenderB);
+        appenders.add(appenderC);
+
+        loggerManager.addAppenders(context, appenders);
+
+        loggerManager.log(TAG, LOG.PRIORITY.FATAL, TEXT);
+
+        final String log = String.format(LOG_FORMAT_4ARGS, TAG, getObjectHash(TAG), getCurrentThreadName(), LogAppenderUtils.getLogString(TEXT));
+
+        verify(appenderA, times(1)).log(LOG.PRIORITY.FATAL, null, log);
+        verify(appenderB, times(1)).log(LOG.PRIORITY.FATAL, null, log);
+        verify(appenderC, times(1)).log(LOG.PRIORITY.FATAL, null, log);
+    }
+
+    @Test
+    public void testInfoLog() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+        List<ILogAppender> appenders = new ArrayList<>();
+
+        ILogAppender appenderA = mockAppender("A");
+        ILogAppender appenderB = mockAppender("B");
+        ILogAppender appenderC = mockAppender("C");
+
+        appenders.add(appenderA);
+        appenders.add(appenderB);
+        appenders.add(appenderC);
+
+        loggerManager.addAppenders(context, appenders);
+
+        loggerManager.log(TAG, LOG.PRIORITY.INFO, TEXT);
+
+        final String log = String.format(LOG_FORMAT_4ARGS, TAG, getObjectHash(TAG), getCurrentThreadName(), LogAppenderUtils.getLogString(TEXT));
+
+        verify(appenderA, times(1)).log(LOG.PRIORITY.INFO, null, log);
+        verify(appenderB, times(1)).log(LOG.PRIORITY.INFO, null, log);
+        verify(appenderC, times(1)).log(LOG.PRIORITY.INFO, null, log);
+    }
+
+
+    @Test
+    public void testVerboseLog() {
+        Context context = mock(Context.class);
+
+        LoggerManager loggerManager = new LoggerManager(mPackageName);
+        List<ILogAppender> appenders = new ArrayList<>();
+
+        ILogAppender appenderA = mockAppender("A");
+        ILogAppender appenderB = mockAppender("B");
+        ILogAppender appenderC = mockAppender("C");
+
+        appenders.add(appenderA);
+        appenders.add(appenderB);
+        appenders.add(appenderC);
+
+        loggerManager.addAppenders(context, appenders);
+
+        loggerManager.log(TAG, LOG.PRIORITY.VERBOSE, TEXT);
+
+        final String log = String.format(LOG_FORMAT_4ARGS, TAG, getObjectHash(TAG), getCurrentThreadName(), LogAppenderUtils.getLogString(TEXT));
+
+        verify(appenderA, times(1)).log(LOG.PRIORITY.VERBOSE, null, log);
+        verify(appenderB, times(1)).log(LOG.PRIORITY.VERBOSE, null, log);
+        verify(appenderC, times(1)).log(LOG.PRIORITY.VERBOSE, null, log);
+    }
+
+    private ILogAppender mockAppender(String analyticsId) {
+        ILogAppender appender = mock(ILogAppender.class);
+
+        when(appender.getLoggerId()).thenReturn(analyticsId);
+
+        return appender;
+    }
+}
