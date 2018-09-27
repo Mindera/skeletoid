83c2f320497b94a9721060abc9c27c630ae920ce
Pedro Vicente <pedro.vicente@mindera.com>
2018-09-27 16:39:56 +0100

    RxBindings: Added module to support extended rxbindings

---
+++ rxbindings/src/main/java/com/mindera/skeletoid/rxbindings/searchview/SearchViewFocusChangeObservable.java
@@ -0,0 +1,54 @@
+package com.mindera.skeletoid.rxbindings.searchview;
+
+import android.view.View;
+import android.widget.SearchView;
+
+import com.jakewharton.rxbinding2.InitialValueObservable;
+
+import io.reactivex.Observer;
+import io.reactivex.android.MainThreadDisposable;
+
+class SearchViewFocusChangeObservable extends InitialValueObservable<Boolean> {
+
+    private final SearchView view;
+
+    SearchViewFocusChangeObservable(SearchView view) {
+        this.view = view;
+    }
+
+    @Override
+    protected void subscribeListener(Observer<? super Boolean> observer) {
+        Listener listener = new Listener(view, observer);
+        observer.onSubscribe(listener);
+        view.setOnQueryTextFocusChangeListener(listener);
+    }
+
+    @Override
+    protected Boolean getInitialValue() {
+        return view.hasFocus();
+    }
+
+    static final class Listener extends MainThreadDisposable
+            implements SearchView.OnFocusChangeListener {
+
+        private final SearchView view;
+        private final Observer<? super Boolean> observer;
+
+        Listener(SearchView view, Observer<? super Boolean> observer) {
+            this.view = view;
+            this.observer = observer;
+        }
+
+        @Override
+        public void onFocusChange(View v, boolean hasFocus) {
+            if (!isDisposed()) {
+                observer.onNext(hasFocus);
+            }
+        }
+
+        @Override
+        protected void onDispose() {
+            view.setOnFocusChangeListener(null);
+        }
+    }
+}
