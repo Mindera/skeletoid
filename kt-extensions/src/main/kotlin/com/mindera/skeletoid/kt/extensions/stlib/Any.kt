2ffa1e143706cbe6201078b1e9108af3ccb02051
Pedro Vicente <pedro.vicente@mindera.com>
2018-10-30 18:24:35 +0000

    Add kotlin extension for Any

---
+++ kt-extensions/src/main/kotlin/com/mindera/skeletoid/kt/extensions/stlib/Any.kt
@@ -0,0 +1,12 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package com.mindera.skeletoid.kt.extensions.stlib
+
+
+inline fun Any?.isNullOrTrue(): Boolean {
+    return this != false
+}
+
+inline fun Any?.isNullOrFalse(): Boolean {
+    return this != true
+}
