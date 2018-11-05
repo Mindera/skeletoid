13518b709b14d6284c63ee6330ad6a9fc6f70f7c
Pedro Vicente <pedro.vicente@mindera.com>
2018-11-05 16:53:44 +0000

    Kt - Any extensions - Improved code readability

--- kt-extensions/src/main/kotlin/com/mindera/skeletoid/kt/extensions/stlib/Any.kt
+++ kt-extensions/src/main/kotlin/com/mindera/skeletoid/kt/extensions/stlib/Any.kt
@@ -2,19 +2,18 @@
 
 package com.mindera.skeletoid.kt.extensions.stlib
 
-
-inline fun Any?.isNullOrTrue(): Boolean {
-    return this != false
-}
-
-inline fun Any?.isNullOrFalse(): Boolean {
-    return this != true
-}
-
-inline fun Any?.isNotNullAndTrue(): Boolean {
+inline fun Any?.isTrueAndNotNull(): Boolean {
     return this == true
 }
 
-inline fun Any?.isNotNullAndFalse(): Boolean {
+inline fun Any?.isFalseAndNotNull(): Boolean {
     return this == false
+}
+
+inline fun Any?.isTrueOrNull(): Boolean {
+    return this != false
+}
+
+inline fun Any?.isFalseOrNull(): Boolean {
+    return this != true
 }
