fe17a65d305242890522a167427ca04312fb8b00
João Manuel Vieira Caseiro <>
2018-12-13 15:29:01 +0000

    Added optional animation to CloseCommandActivity

--- base/src/main/java/com/mindera/skeletoid/routing/CloseActivityCommand.java
+++ base/src/main/java/com/mindera/skeletoid/routing/CloseActivityCommand.java
@@ -8,13 +8,24 @@ import android.app.Activity;
 public class CloseActivityCommand implements IRouteCommand {
 
     private final Activity mActivity;
+    private int enterAnimation = 0;
+    private int exitAnimation = 0;
 
     public CloseActivityCommand(Activity activity) {
         mActivity = activity;
     }
 
+    public CloseActivityCommand(Activity activity, int enterAnimation, int exitAnimation) {
+        this(activity);
+        this.enterAnimation = enterAnimation;
+        this.exitAnimation = exitAnimation;
+    }
+
     @Override
     public void navigate() {
         mActivity.finish();
+        if(enterAnimation != 0 || exitAnimation != 0) {
+            mActivity.overridePendingTransition(enterAnimation, exitAnimation);
+        }
     }
 }
