f698360c501405f91cabb83cac08b34e99d82a5c
João Manuel Vieira Caseiro <joao.caseiro@mindera.com>
2018-12-20 14:33:30 +0000

    Added JvmOverloads and removed unnecessary constructor

--- base/src/main/java/com/mindera/skeletoid/routing/CloseActivityCommand.kt
+++ base/src/main/java/com/mindera/skeletoid/routing/CloseActivityCommand.kt
@@ -5,9 +5,7 @@ import android.app.Activity
 /**
  * Closes the activity passed in the constructor
  */
-class CloseActivityCommand(private val activity: Activity, private val enterAnimation: Int = 0, private val exitAnimation: Int = 0) : IRouteCommand {
-
-    constructor(context: Activity) : this(activity = context)
+class CloseActivityCommand @JvmOverloads constructor(private val activity: Activity, private val enterAnimation: Int = 0, private val exitAnimation: Int = 0) : IRouteCommand {
 
     override fun navigate() {
         activity.finish()
