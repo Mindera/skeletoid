3b1a13bf44a5cb49b879204506c5b9c3808f3000
João Caseiro <38102360+Jmvcaseiro@users.noreply.github.com>
2019-01-02 15:37:09 +0000

    Close activity command animated (#75)
    * Added optional animation to CloseCommandActivity
    
    * Refactored to kotlin
    
    * Added JvmOverloads and removed unnecessary constructor

---
+++ base/src/main/java/com/mindera/skeletoid/routing/CloseActivityCommand.kt
@@ -0,0 +1,16 @@
+package com.mindera.skeletoid.routing
+
+import android.app.Activity
+
+/**
+ * Closes the activity passed in the constructor
+ */
+class CloseActivityCommand @JvmOverloads constructor(private val activity: Activity, private val enterAnimation: Int = 0, private val exitAnimation: Int = 0) : IRouteCommand {
+
+    override fun navigate() {
+        activity.finish()
+        if (enterAnimation != 0 || exitAnimation != 0) {
+            activity.overridePendingTransition(enterAnimation, exitAnimation)
+        }
+    }
+}
