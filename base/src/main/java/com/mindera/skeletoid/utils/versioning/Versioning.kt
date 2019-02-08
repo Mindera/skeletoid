31626b130fc05e6e6fa9606e5f11fe2e667a9c7a
Bruno Garces <bruno.garces@mindera.com>
2019-02-08 16:35:06 +0000

    Update base/src/main/java/com/mindera/skeletoid/utils/versioning/Versioning.kt
    Co-Authored-By: neteinstein <neteinstein@gmail.com>

--- base/src/main/java/com/mindera/skeletoid/utils/versioning/Versioning.kt
+++ base/src/main/java/com/mindera/skeletoid/utils/versioning/Versioning.kt
@@ -31,7 +31,16 @@ object Versioning {
             i++
         }
         // compare first non-equal ordinal number
-        return if (i < vals1.size && i < vals2.size) {
+        return run {
+            // compare first non-equal ordinal number
+            if (i < vals1.size && i < vals2.size) {
+                vals1[i].toInt().compareTo(vals2[i].toInt())
+            } else {
+                // the strings are equal or one string is a substring of the other
+                // e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
+                vals1.size - vals2.size
+            }
+        }.sign
             val diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]))
             Integer.signum(diff)
         } else {
