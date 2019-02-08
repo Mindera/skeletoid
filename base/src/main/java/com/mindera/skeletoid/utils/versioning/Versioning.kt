d96343454a04cd46151378e77986bd1815a32422
Pedro Vicente <neteinstein@gmail.com>
2019-02-08 17:01:25 +0000

    Feature - Added versioning utils (#78)
    * Feature - Added versioning utils
    
    * Versioning - Added unit tests
    
    Co-Authored-By: neteinstein <neteinstein@gmail.com>

---
+++ base/src/main/java/com/mindera/skeletoid/utils/versioning/Versioning.kt
@@ -0,0 +1,49 @@
+package com.mindera.skeletoid.utils.versioning
+
+import kotlin.math.sign
+
+
+object Versioning {
+    /**
+     * Compares two version strings.
+     * Credits to: https://gist.github.com/antalindisguise/d9d462f2defcfd7ae1d4
+     *
+     * Use this instead of String.compareTo() for a non-lexicographical
+     * comparison that works for version strings. e.g. "1.10".compareTo("1.6").
+     *
+     * @note It does not work if "1.10" is supposed to be equal to "1.10.0".
+     *
+     * @param str1 a string of ordinal numbers separated by decimal points.
+     * @param str2 a string of ordinal numbers separated by decimal points.
+     * @return The result is a negative integer if str1 is _numerically_ less than str2.
+     *         The result is a positive integer if str1 is _numerically_ greater than str2.
+     *         The result is zero if the strings are _numerically_ equal.
+     */
+    fun compareVersions(str1: String, str2: String): Int {
+        if (str1.isBlank() || str2.isBlank()) {
+            throw IllegalArgumentException("Invalid Version")
+        }
+
+        val vals1 = str1.split("\\.".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        val vals2 = str2.split("\\.".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+
+        var i = 0
+        // set index to first non-equal ordinal or length of shortest version string
+        while (i < vals1.size && i < vals2.size && vals1[i] == vals2[i]) {
+            i++
+        }
+        // compare first non-equal ordinal number
+        return run {
+            // compare first non-equal ordinal number
+            if (i < vals1.size && i < vals2.size) {
+                vals1[i].toInt().compareTo(vals2[i].toInt())
+            } else {
+                // the strings are equal or one string is a substring of the other
+                // e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
+                vals1.size - vals2.size
+            }
+        }.sign
+
+    }
+
+}
---
+++ base/src/test/java/com/mindera/skeletoid/utils/VersioningUnitTest.kt
@@ -0,0 +1,61 @@
+package com.mindera.skeletoid.utils
+
+import com.mindera.skeletoid.utils.versioning.Versioning
+import org.junit.Test
+
+
+class VersioningUnitTest{
+
+
+    @Test
+    fun testVersioningEqual() {
+        val equals = Versioning.compareVersions("1.0.0", "1.0.0")
+        assert(equals == 0)
+    }
+
+    @Test
+    fun testVersioningBiggerBuild() {
+        val equals = Versioning.compareVersions("1.0.0", "1.0.1")
+        assert(equals < 0)
+    }
+
+    @Test
+    fun testVersioningBiggerMinor() {
+        val equals = Versioning.compareVersions("1.0.0", "1.1.0")
+        assert(equals < 0)
+    }
+
+    @Test
+    fun testVersioningBiggerMajor() {
+        val equals = Versioning.compareVersions("1.0.0", "2.0.0")
+        assert(equals < 0)
+    }
+
+    @Test
+    fun testVersioningSmallerBuild() {
+        val equals = Versioning.compareVersions("1.0.1", "1.0.0")
+        assert(equals > 0)
+    }
+
+    @Test
+    fun testVersioningSmallerMinor() {
+        val equals = Versioning.compareVersions("1.1.0","1.0.0")
+        assert(equals > 0)
+    }
+
+    @Test
+    fun testVersioningSmallerMajor() {
+        val equals = Versioning.compareVersions("2.0.0", "1.0.0")
+        assert(equals > 0)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testVersioningWrongInput() {
+        Versioning.compareVersions("", "")
+    }
+
+    @Test(expected = NumberFormatException::class)
+    fun testVersioningWrongInputNonNumeric() {
+        Versioning.compareVersions("a", "b")
+    }
+}
