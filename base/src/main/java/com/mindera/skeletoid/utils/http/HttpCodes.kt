70dc1d34abbb7ea6c5d28a6885b6fe8ef645e58e
Bruno Garcês <bruno.garces@mindera.com>
2019-04-22 18:28:30 +0100

    Adds new HTTP conflcit code.

--- base/src/main/java/com/mindera/skeletoid/utils/http/HttpCodes.kt
+++ base/src/main/java/com/mindera/skeletoid/utils/http/HttpCodes.kt
@@ -4,6 +4,7 @@ object HttpCodes {
     const val HTTP_BAD_REQUEST = 400
     const val HTTP_UNAUTHORIZED = 401
     const val HTTP_FORBIDDEN = 403
+    const val HTTP_CONFLICT = 409
     const val HTTP_GONE = 410
 
     const val SERVER_UNAVAILABLE = 503
