92e7c1882c68ab704c0ac83d8fb21bc43d64a04f
Daniel Sousa <daniel.sousa@mindera.com>
2019-05-29 16:06:59 +0100

    Rename file to tuples.kt and add sextuple.

--- base/src/main/java/com/mindera/skeletoid/utils/tuples/Tuples.kt
+++ base/src/main/java/com/mindera/skeletoid/utils/tuples/Tuples.kt
@@ -14,3 +14,12 @@ data class Quintuple<out A, out B, out C, out D, out E>(
         val fourth: D,
         val fifth: E
 )
+
+data class Sextuple<out A, out B, out C, out D, out E, out F>(
+        val first: A,
+        val second: B,
+        val third: C,
+        val fourth: D,
+        val fifth: E,
+        val sixth: F
+)
