f520e700c79cfc4ba0e18dc7946497e973355030
Rita Ferreira <a.rita.ferreira@mindera.com>
2019-06-04 17:56:53 +0100

    Added comments to the Controller

--- apprating/src/main/java/com/mindera/skeletoid/apprating/controller/AppRatingController.kt
+++ apprating/src/main/java/com/mindera/skeletoid/apprating/controller/AppRatingController.kt
@@ -13,11 +13,24 @@ class AppRatingController {
     private var countsPerTimeInterval: Pair<Int, Long>? = null
     private var promptTimeInterval: Long? = null
 
+    /**
+     * Sets up the conditions to prompt the dialog.
+     *
+     * @param countsPerTimeInterval Pair<Int, Long> values with the maximum number of times the dialog can be prompt per time range (in days)
+     * @param promptTimeInterval Time distance between prompts (in days)
+     */
     fun setupConditions(countsPerTimeInterval: Pair<Int, Long>, promptTimeInterval: Long) {
         this.countsPerTimeInterval = countsPerTimeInterval
         this.promptTimeInterval = promptTimeInterval
     }
 
+    /**
+     * Checks if the data in the store matches the conditions and updates the values in the store.
+     *
+     * @param context Context
+     * @param dialogResultCallback Callback to handle default dialog response
+     * @return true if the conditions were met, and the dialog can be prompted.
+     */
     fun promptDialog(context: Context, dialogResultCallback: AppRatingDialogResponseCallback? = null): Boolean {
         val store = AppRatingStore(context)
         return shouldPromptDialog(store).also {
@@ -31,6 +44,12 @@ class AppRatingController {
         }
     }
 
+    /**
+     * Handles the response of the dialog by updating the values in the store.
+     *
+     * @param context Context
+     * @param response Selected response to the dialog
+     */
     fun handleDialogResponse(context: Context, response: DialogResponse) {
         val store = AppRatingStore(context)
         when (response) {
@@ -43,6 +62,12 @@ class AppRatingController {
         }
     }
 
+    /**
+     * Checks if the values in the store matches the conditions to prompt the dialog.
+     *
+     * @param store Store that has the values related with the app rating conditions
+     * @return true if the conditions were met
+     */
     private fun shouldPromptDialog(store: AppRatingStore): Boolean {
         return if (store.initialPromptDate.isEmpty()) {
             store.initialPromptDate = DateUtils.formatDate(Date())
@@ -56,6 +81,12 @@ class AppRatingController {
         }
     }
 
+    /**
+     * Checks if already reached the maximum amount of time that the rating dialog can be shown in a certain period of time.
+     *
+     * @param store Store that has the values related with the app rating conditions
+     * @return true if the conditions were met
+     */
     private fun checkCountPerTimeCondition(store: AppRatingStore): Boolean {
         return countsPerTimeInterval?.let { (count, range) ->
             val initialPromptDate = DateUtils.parseDate(store.initialPromptDate)
@@ -71,6 +102,12 @@ class AppRatingController {
         } ?: true
     }
 
+    /**
+     * Checks if a certain range of time has passed since the last dialog was prompted.
+     *
+     * @param store Store that has the values related with the app rating conditions
+     * @return true if the conditions were met
+     */
     private fun checkPromptTimeIntervalCondition(store: AppRatingStore): Boolean {
         return promptTimeInterval?.let {
             val lastTimeDate = DateUtils.parseDate(store.lastTimePrompted)
