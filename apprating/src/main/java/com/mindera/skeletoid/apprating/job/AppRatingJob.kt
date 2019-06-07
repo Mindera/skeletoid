3c54acf08afb703f0a01165561bb7b206ba601f6
Rita Ferreira <a.rita.ferreira@mindera.com>
2019-06-07 10:14:45 +0100

    Updated libs according to androidX

--- apprating/src/main/java/com/mindera/skeletoid/apprating/job/AppRatingJob.kt
+++ apprating/src/main/java/com/mindera/skeletoid/apprating/job/AppRatingJob.kt
@@ -1,12 +1,14 @@
 package com.mindera.skeletoid.apprating.job
 
+import android.content.Context
 import androidx.work.Worker
+import androidx.work.WorkerParameters
 
-class AppRatingJob: Worker() {
+class AppRatingJob(context: Context, workerParameters: WorkerParameters): Worker(context, workerParameters) {
 
     override fun doWork(): Result {
         //For now this is doing nothing, just acting like a timer
         //for the activity to listen to the state and prompt a dialog whenever it finishes
-        return Result.SUCCESS
+        return Result.success()
     }
 }
