302e8448a368b6fde7cdc3dbf8fac2dff0b934bd
Rita Ferreira <a.rita.ferreira@mindera.com>
2019-06-13 15:53:29 +0100

    Added log to AppRatingJob

--- apprating/src/main/java/com/mindera/skeletoid/apprating/job/AppRatingJob.kt
+++ apprating/src/main/java/com/mindera/skeletoid/apprating/job/AppRatingJob.kt
@@ -1,6 +1,7 @@
 package com.mindera.skeletoid.apprating.job
 
 import android.content.Context
+import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
@@ -16,6 +17,7 @@ class AppRatingJob(context: Context, workerParameters: WorkerParameters): Worker
     override fun doWork(): Result {
         //For now this is doing nothing, just acting like a timer
         //for the activity to listen to the state and prompt a dialog whenever it finishes
+        Log.d(JOB_TAG, "Rating work finishing...")
         return Result.success()
     }
 }
