be3d65d061dfd1d71bdb4c8156fe23237a78d232
Rita Ferreira <a.rita.ferreira@mindera.com>
2019-06-25 14:07:42 +0100

    Changed open play store command intent

--- base/src/main/java/com/mindera/skeletoid/routing/OpenGooglePlayCommand.kt
+++ base/src/main/java/com/mindera/skeletoid/routing/OpenGooglePlayCommand.kt
@@ -3,18 +3,23 @@ package com.mindera.skeletoid.routing
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import com.mindera.skeletoid.R
 
 class OpenGooglePlayCommand(
     private val context: Context,
-    private val googlePlayUrl: Int,
     private val packageName: Int
 ) : IRouteCommand {
 
     override fun navigate() {
-        val intent = Intent(Intent.ACTION_VIEW).apply {
-            data = Uri.parse(context.getString(googlePlayUrl, context.getString(packageName)))
-        }
+        var url = context.getString(R.string.play_store_app_deep_link, context.getString(packageName))
 
-        context.startActivity(intent)
+        try {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            url = context.getString(R.string.play_store_app_url, context.getString(packageName))
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            context.startActivity(intent)
+        }
     }
 }
