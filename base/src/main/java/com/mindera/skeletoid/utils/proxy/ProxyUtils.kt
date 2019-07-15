f2970031d71e9f5ffcce0c0f6e22cbb4263dbc1a
Daniel Sousa <daniel.sousa@mindera.com>
2019-07-15 16:30:07 +0100

    [fix] Adds ProxyUtils

---
+++ base/src/main/java/com/mindera/skeletoid/utils/proxy/ProxyUtils.kt
@@ -0,0 +1,18 @@
+package com.mindera.skeletoid.utils.proxy
+
+import com.mindera.skeletoid.logs.LOG
+
+object ProxyUtils {
+    private const val LOG_TAG = "ProxyUtils"
+
+    fun getProxyDetails(): String? {
+        var proxyAddress: StringBuilder? = null
+        try {
+            System.getProperty("http.proxyHost")?.run { proxyAddress = StringBuilder(this) }
+            proxyAddress?.apply { this.append(":${System.getProperty("http.proxyPort")}") }
+        } catch (exception: Exception) {
+            LOG.e(LOG_TAG, exception, "Get proxy details exception: ${exception.message}")
+        }
+        return proxyAddress?.toString()
+    }
+}
