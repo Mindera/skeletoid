ef59ca572caa1ec5493b2a42a7d38c0b24e61499
Daniel Sousa <djrsousa@gmail.com>
2019-08-29 14:39:27 +0100

    [feature] Adds a new parameter to differentiate the internal app navigation from external deep links. (#113)

--- base/src/main/java/com/mindera/skeletoid/routing/OpenUriCommand.kt
+++ base/src/main/java/com/mindera/skeletoid/routing/OpenUriCommand.kt
@@ -6,9 +6,14 @@ import android.net.Uri
 
 class OpenUriCommand(private val context: Context, private val deepLink: String) : IRouteCommand {
 
+    companion object {
+        const val IS_INTERNAL_APP_NAVIGATION_EXTRA = "IS_INTERNAL_APP_NAVIGATION_EXTRA"
+    }
+
     override fun navigate() {
         val intent = Intent(Intent.ACTION_VIEW)
         intent.data = Uri.parse(deepLink)
+        intent.putExtra(IS_INTERNAL_APP_NAVIGATION_EXTRA, true)
         context.startActivity(intent)
     }
 }
