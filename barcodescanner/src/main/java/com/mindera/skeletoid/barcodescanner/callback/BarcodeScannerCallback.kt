540f388ca08cba3f636f0a8723bc6d9764921410
Rui Almeida <34213568+ruialmeida51@users.noreply.github.com>
2019-09-17 17:27:17 +0100

    [feature] Barcode Scanner (#114)
    * [feature] barcode scanner - uses google api vision to create an easy way to scan all types of barcodes.
    
    * Create README.md
    
    * moved aftermeasured to kotlin ext and changed name to after drawn
    added possibility of overriding request permissions behaviour
    changed linear layout to framelayout
    changed static values to res files
    changed Log to LOG
    
    * Update README.md
    
    * Update README.md
    
    * deleted useless tests
    replaced Log with LOG on missing classes
    fixed R.integer
    improved naming
    removed empty space to cure OCD
    
    * readme & typo
    
    * fixed readme
    fixed ocd spaces
    added b r a c k e t s
    removed wildcard imports

---
+++ barcodescanner/src/main/java/com/mindera/skeletoid/barcodescanner/callback/BarcodeScannerCallback.kt
@@ -0,0 +1,14 @@
+package com.mindera.skeletoid.barcodescanner.callback
+
+import com.google.android.gms.vision.barcode.Barcode
+
+/**
+ * Callback that is called after the reader fragment
+ * detects the correct barcode.
+ */
+interface BarcodeScannerCallback {
+    /**
+     * Returns the closest detected barcode to the center.
+     */
+    fun onBarcodeRead(barcode: Barcode?)
+}
