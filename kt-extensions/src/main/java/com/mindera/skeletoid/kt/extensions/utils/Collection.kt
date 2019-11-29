eb48b5f6140eeccc41c005b7e829670e708fb481
Guilherme Almeida <guilherme.abreu.almeida@gmail.com>
2019-11-29 09:44:26 +0000

    [feature] Added new extension function List.nullIfEmpty().

--- kt-extensions/src/main/java/com/mindera/skeletoid/kt/extensions/utils/Collection.kt
+++ kt-extensions/src/main/java/com/mindera/skeletoid/kt/extensions/utils/Collection.kt
@@ -3,3 +3,7 @@ package com.mindera.skeletoid.kt.extensions.utils
 fun <T: Any> Collection<T>.nullIfEmpty(): Collection<T>? {
     return if (isEmpty()) null else this
 }
+
+fun <T: Any> List<T>.nullIfEmpty(): List<T>? {
+    return if (isEmpty()) null else this
+}
