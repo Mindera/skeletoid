3668ced4e5ac567becf0a5e18d32e2348b993a80
Rita Ferreira <a.rita.ferreira@mindera.com>
2020-03-19 10:45:34 +0000

    Separated Collection utils from List utils

--- kt-extensions/src/main/java/com/mindera/skeletoid/kt/extensions/utils/List.kt
+++ kt-extensions/src/main/java/com/mindera/skeletoid/kt/extensions/utils/List.kt
@@ -1,5 +1,5 @@
 package com.mindera.skeletoid.kt.extensions.utils
 
-fun <T: Any> Collection<T>.nullIfEmpty(): Collection<T>? {
+fun <T: Any> List<T>.nullIfEmpty(): List<T>? {
     return if (isEmpty()) null else this
 }
