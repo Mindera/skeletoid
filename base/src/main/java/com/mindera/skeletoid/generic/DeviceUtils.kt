d6faba0d900c715a4d35aa061f169404d4083e90
Pedro Vicente <pedro.vicente@mindera.com>
2020-06-04 15:19:17 +0100

    [fix] Improved DeviceUtils code

--- base/src/main/java/com/mindera/skeletoid/generic/DeviceUtils.kt
+++ base/src/main/java/com/mindera/skeletoid/generic/DeviceUtils.kt
@@ -5,112 +5,76 @@ import android.os.Build
 object DeviceUtils {
 
     /**
-         * Get installed OS release name
-         *
-         * @return String with the installed OS release version
-         *//**
      * Get installed OS release name
      *
      * @return String with the installed OS release version or empty if not found
      */
-    val oSReleaseVersion: String
-        get() = Build.VERSION.RELEASE ?: ""
-@Deprecated("Use DeviceUtils.osRelease instead") get() {
-            return osRelease
-        }
+    val osRelease: String = Build.VERSION.RELEASE ?: ""
 
     /**
-         * Get the device's manufacturer and model name
-         *
-         * @return String with the device's manufacturer and model name
-         *//**
      * Get the device's brand name
      *
      * @return String with the device's brand name or empty String if not found
      */
-    val deviceBrand: String
-        get() = Build.BRAND ?: ""
-@Deprecated("Use DeviceUtils.brand instead") get() {
-            return brand
-        }
+    val brand: String = Build.BRAND ?: ""
 
     /**
      * Get the device's manufacturer name
      *
      * @return String with the device's manufacturer name or empty String if not found
      */
-    val manufacturer: String
-        get() = Build.MANUFACTURER ?: ""
+    val manufacturer: String = Build.MANUFACTURER ?: ""
 
     /**
      * Get the device's model name
      *
      * @return String with the device's model name or empty String if not found
      */
-    val model: String
-        get() = Build.MODEL ?: ""
+    val model: String = Build.MODEL ?: ""
 
     /**
-         * Get the device's manufacturer and model name
-         *
-         * @return String with the device's manufacturer and model name
-         *//**
      * Get the device's manufacturer and model name
      *
-     * @return String with the device's manufacturer and model name or empty String if not found
+     * @return String with the device's manufacturer and model name
      */
-    val deviceName: String
-        get() = if (model.startsWith(manufacturer)) model else "$manufacturer $model"
-@Deprecated("Use DeviceUtils.name instead") get() {
-            return name
-        }
+
+    val name: String = if (model.startsWith(manufacturer)) model else "$manufacturer $model"
+
 
     /**
-         * Get installed OS SDK version
-         *
-         * @return String with the installed OS SDK version
-         *//**
      * Get installed OS SDK version
      *
      * @return String with the installed OS SDK version or empty String if not found
      */
-    val oSSDKVersion: Int
-        get() = Build.VERSION.SDK_INT
-@Deprecated("Use DeviceUtils.sdkVersion instead") get() {
-            return sdkVersion
-        }
+    val sdkVersion: Int = Build.VERSION.SDK_INT
 
     /**
      * Get the name of the overall product
      *
      * @return String with the name of the overall product or empty String if not found
      */
-    val product: String
-        get() = Build.PRODUCT ?: ""
+    val product: String = Build.PRODUCT ?: ""
 
     /**
      * The name of the hardware (from the kernel command line or /proc)
      *
      * @return String with the name of the hardware or empty String if not found
      */
-    val hardware: String
-        get() = Build.HARDWARE ?: ""
+    val hardware: String = Build.HARDWARE ?: ""
 
     /**
      * The name of the industrial design
      *
      * @return String with the name of the industrial design or empty String if not found
      */
-    val device: String
-        get() = Build.DEVICE ?: ""
+    val device: String = Build.DEVICE ?: ""
 
     /**
      * The OS version code name
      *
      * @return String with the name of the OS version code or empty String if not found
      */
-    val osName: String
-        get() = Build.VERSION_CODES::class.java.fields[sdkVersion].name
+    val osName: String = Build.VERSION_CODES::class.java.fields[sdkVersion].name
 
     val deviceSpecifications: String
         get() = "OS Release: $osRelease \n" +
