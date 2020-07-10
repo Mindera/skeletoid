54f582b2c721e5a855f8f914a3c5f429b7da21ca
Pedro Vicente <pedro.vicente@mindera.com>
2020-07-10 22:13:58 +0100

    [fix] Fixed unit tests

--- base/src/test/java/com/mindera/skeletoid/threads/threadpools/ScheduledThreadPoolUnitTest.kt
+++ base/src/test/java/com/mindera/skeletoid/threads/threadpools/ScheduledThreadPoolUnitTest.kt
@@ -3,8 +3,12 @@ package com.mindera.skeletoid.threads.threadpools
 import com.mindera.skeletoid.utils.extensions.mock
 import org.junit.Assert
 import org.junit.Test
+import org.mockito.Mockito
+import java.util.concurrent.ArrayBlockingQueue
 
 class ScheduledThreadPoolUnitTest {
+
+
     @Test
     fun testThreadPoolInitialization() {
         val namedThreadFactory : NamedThreadFactory = mock()
@@ -15,7 +19,7 @@ class ScheduledThreadPoolUnitTest {
 
     @Test
     fun testShutdownThreadPool() {
-        val namedThreadFactory : NamedThreadFactory = mock()
+        val namedThreadFactory : NamedThreadFactory = NamedThreadFactory("",1)
         val scheduledThreadPoolExecutor = ScheduledThreadPoolExecutor(1, namedThreadFactory)
         scheduledThreadPoolExecutor.shutdown()
         Assert.assertTrue(scheduledThreadPoolExecutor.isShutdown)
@@ -23,7 +27,7 @@ class ScheduledThreadPoolUnitTest {
 
     @Test
     fun testShutdownNowThreadPool() {
-        val namedThreadFactory : NamedThreadFactory = mock()
+        val namedThreadFactory : NamedThreadFactory = NamedThreadFactory("",1)
         val scheduledThreadPoolExecutor = ScheduledThreadPoolExecutor(1, namedThreadFactory)
         scheduledThreadPoolExecutor.shutdownNow()
         Assert.assertTrue(scheduledThreadPoolExecutor.isShutdown)
