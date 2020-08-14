0d18899e431a6818ee37e89b5f610856002ec56c
Pedro Vicente <pedro.vicente@mindera.com>
2020-08-14 14:53:53 +0100

    [fix] Added stacktrace logging

--- rxjava/src/main/kotlin/com/mindera/skeletoid/rxjava/bindings/interfaces/IRxBindings.kt
+++ rxjava/src/main/kotlin/com/mindera/skeletoid/rxjava/bindings/interfaces/IRxBindings.kt
@@ -4,7 +4,15 @@ interface IRxBindings {
 
     fun setupRxBindings()
 
+    /**
+     * Clear is meant to be used onPause for example, just to clear current disposables that can
+     * be reused. This follows the same pattern as the clear() method of disposables
+     */
     fun clearRxBindings()
 
+    /**
+     * Dispose is meant to be used on final states: like onStop / onDestroy it's irreversible
+     * This follows the same pattern as the dispose() method of disposables
+     */
     fun disposeRxBindings()
 }
